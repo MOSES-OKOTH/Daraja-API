8110290ee1ff47bd4dd99aef8be94d67664fc5c7
MOSES-OKOTH <mosesokoth164@gmail.com>
2023-09-28 18:26:25 +0300

    Modified the stk push file

--- stkpush.php
+++ stkpush.php
@@ -2,7 +2,7 @@
 include "auth.php";
 
 $processrequestUrl = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
-$callbackUrl = "https://a2d6-41-139-192-199.ngrok-free.app/Daraja%20API/callback.php"; //getting the link from ngrok
+$callbackUrl = ""; //get the link from ngrok
 $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
 $businessShortCode = 174379;
 $timestamp = date("YmdHis");
